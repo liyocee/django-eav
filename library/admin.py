4cddc6e16ab2b6d4ee22e42b78d073a634a5499a
Liyosi Collins <collinskivale@gmail.com>
2016-02-13 20:51:24 +0300

    customize how the Books model is registered in the admin side

--- library/admin.py
+++ library/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from eav.forms import BaseDynamicEntityForm
+from eav.admin import BaseEntityAdmin
 from .models import Library, Book
 
+
+class BookAdminForm(BaseDynamicEntityForm):
+    # create  custom form to show eav eav attributes when adding a book
+    model = Book
+
+
+class BookAdmin(BaseEntityAdmin):
+    form = BookAdminForm
+
 admin.site.register(Library)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
