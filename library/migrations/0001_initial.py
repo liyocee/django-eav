0905ae1dd48e20d87755939f9a0632defbf96041
Liyosi Collins <collinskivale@gmail.com>
2016-02-13 20:25:40 +0300

    added library models

---
+++ library/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=128)),
+                ('description', models.TextField(null=True, blank=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Library',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=128)),
+                ('books', models.ManyToManyField(to='library.Book')),
+            ],
+        ),
+    ]
--- library/models.py
+++ library/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    author = models.ForeignKey(User)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        app_label = 'library'
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    books = models.ManyToManyField(Book)
+
+    class Meta:
+        app_label = 'library'
