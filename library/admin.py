d165c5c9c22562780eb0e1b33c7935f15aa2791c
Liyosi Collins <collinskivale@gmail.com>
2016-02-13 20:33:20 +0300

    make models printable

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Library, Book
 
-# Register your models here.
+admin.site.register(Library)
+admin.site.register(Book)
--- library/models.py
+++ library/models.py
@@ -7,6 +7,10 @@ class Book(models.Model):
     author = models.ForeignKey(User)
     description = models.TextField(null=True, blank=True)
 
+    def __unicode__(self):
+        return "{name} - {author}".format(
+            name=self.name, author=self.author.get_username())
+
     class Meta:
         app_label = 'library'
 
@@ -14,6 +18,9 @@ class Book(models.Model):
 class Library(models.Model):
     name = models.CharField(max_length=128, null=False, blank=False)
     books = models.ManyToManyField(Book)
+
+    def __unicode__(self):
+        return self.name
 
     class Meta:
         app_label = 'library'
